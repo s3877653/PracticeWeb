c42d1a0f8b19c18b9bf6461543307aa1eb0b1ead
Khoi Ho <khoi.ho@nois.vn>
2023-02-27 18:53:01 -0800

    add filter movie by nation

--- RazorLoginPage/Pages/FilteredPage/FilteredMoviePage.cshtml.cs
+++ RazorLoginPage/Pages/FilteredPage/FilteredMoviePage.cshtml.cs
@@ -41,17 +41,17 @@ namespace RazorLoginPage.Pages.FilteredPage
 			new Movie(11,"pokemon2","Full HD vietsub","phim hoat hinh vui",true,2020,catergories2,"Nam","","","1h20p","nhat ban",9.8,actors,1_232_221),
 			new Movie(12,"doraemon 2","Full HD","phim hoat hinh tuoi tho",false,2022,catergories4,"Na","24","24","24 phut","nhat ban",9.5,actors,1_200_300),
 			new Movie(13," tantay du ki","Full HD","tan tay du ki2",false,1990,catergories3,"BruceWang","95","100","30 phut","trung quoc",10.0,actors,9_600_400),
-			new Movie(14,"nha ba nu 2","Full HD","phim tran thanh",true,2026,catergories3,"Tran thanh","","","2h20p","viet nam",8.0,actors,0),
+			new Movie(14,"nha ba nu 2","Full HD","phim tran thanh",true,2026,catergories3,"Tran thanh","","","2h20p","han quoc",8.0,actors,0),
 			new Movie(15,"pursue of happiness2","Full HD","phim document",true,2023,catergories1,"BruceWang","","","2h10p","my",10.0,actors,4_400_400),
 			new Movie(16,"avatar 4","Full HD engsub","phim hay",true,2026,catergories1,"BruceWang","","","3h10p","my",10.0,actors,0),
-			new Movie(17,"naruto shipuden","Full HD engsub","phim anime",false,2012,catergories4,"ken","269","300","24p","nhat ban",10.0,actors,563_700),
+			new Movie(17,"naruto shipuden","Full HD engsub","phim anime",false,2012,catergories4,"ken","269","300","24p","han quoc",10.0,actors,563_700),
 			new Movie(18,"Breaking bad2","Full HD vietsub","phim hay",true,2020,catergories1,"BruceWang","25","25","30p","my",8.5,actors,563_800),
 			new Movie(19,"2025","Full HD vietsub","phim tan the",true,2023,catergories1,"BruceWang","","","3h10p","my",9.9,actors,563_365),
 			new Movie(20,"walking dead2","Full HD vietsub","phim xac song",true,2012,catergories1,"BruceWang","","","3h10p","my",9.5,actors,5_000_000)
 		};
 		[BindProperty]
 		public int MovieID { get; set; }
-		public string Category { get; set; }
+		public string FilterString { get; set; }
 		[BindProperty(SupportsGet = true)]
 		public string StringSearch { get; set; }
 		public List<Movie> FilteredMovie = new();
@@ -65,7 +65,7 @@ namespace RazorLoginPage.Pages.FilteredPage
 		public void OnGet(string filterString = "")
         {
 			FilteredMovie = _searchService.SearchMovieService(movieList, StringSearch);
-			Category = filterString;
+			FilterString = filterString;
         }
 		public IActionResult OnPostMovieHandler()
 		{
