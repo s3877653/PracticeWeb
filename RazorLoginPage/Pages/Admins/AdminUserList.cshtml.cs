2c721ca94d9891d57e59de67ff9a0e039244a01c
Khoi Ho <khoi.ho@nois.vn>
2023-02-28 23:01:18 -0800

    add search bar in admin user page

--- RazorLoginPage/Pages/Admins/AdminUserList.cshtml.cs
+++ RazorLoginPage/Pages/Admins/AdminUserList.cshtml.cs
@@ -11,16 +11,32 @@ namespace RazorLoginPage.Pages.Users
     {
         private readonly ApplicationDBContext _dbContext;
         public IEnumerable<User> Users { get; set; }
-        public User CurrentUser { get; set; }
+		public IEnumerable<User> FilteredUsers { get; set; }
+		public User CurrentUser { get; set; }
+        [BindProperty]
+        public string SearchString { get; set; }
 
         public IndexModel(ApplicationDBContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        public void OnGet()
-        {
-            Users = _dbContext.Users;           			           
+        public void OnGet(string searchString="")
+        {			
+			Console.WriteLine(searchString);
+			if (!string.IsNullOrEmpty(searchString))
+            {
+				Console.WriteLine("testzzzzzzzzzzzzzz");
+				Users = _dbContext.Users.Where(u => u.UserName.Contains(searchString)
+											   || u.Email.Contains(searchString)).ToList();				               
+            }
+            else
+            {
+				Console.WriteLine("test1");
+				
+				Users = _dbContext.Users;
+
+			}                                              			           
 		}
         
         public async Task<IActionResult> OnPostDeleteHandler(int id)
